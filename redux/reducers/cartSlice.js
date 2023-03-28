80e61ed2b79a4c2651ca062c315e73ce30209573
Sujokum <suneelahmed413@gmail.com>
2023-03-28 20:41:42 +0500

    dgfdgfd

--- redux/reducers/cartSlice.js
+++ redux/reducers/cartSlice.js
@@ -21,12 +21,38 @@ const cartSlice  = createSlice({
             }
         },
        remove (state , action){
-        return state.filter((val)=> val.id !== action.payload); 
+        state.cartProduct = state.cartProduct.filter((item) => item.id !== action.payload);
        },
-       increment (state , action){
-        return
-       }
-
+       incrementCartItem (state , action){
+        state.cartProduct = state.cartProduct.map((item) => {
+            if (item.id === action.payload) {
+              return { ...item, quantity: item.quantity + 1 }; 
+            }
+            return item;
+          });
+    },
+       decrementCartItem (state , action){
+        state.cartProduct = state.cartProduct.map((item) => {
+            if (item.id === action.payload) {
+              return { ...item, quantity: item.quantity - 1 };
+            }
+            return item;
+          });
+    },
+    getCartTotal (state){
+        let {totalQuantity , totalPrice} = state.cartProduct.reduce((cartTotal , cartItem)=>{
+            const {price , quantity} = cartItem;
+            const itemTotal = price * quantity;
+            cartTotal.totalPrice += itemTotal;
+            cartTotal.totalQuantity += quantity;
+            return cartTotal;
+        },{
+            totalQuantity : 0,
+            totalPrice :0 
+        });
+        state.totalPrice = parseInt(totalPrice.toFixed(2));
+        state.totalQuantity = totalQuantity;
+    }
     }
 
 })
